2edf5c2e81a6190703eb6845284e522b887ad40d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-13 00:34:45 +0500

    add switch_region func

--- pages/roles/user/user_appeal.py
+++ pages/roles/user/user_appeal.py
@@ -1,7 +1,29 @@
+from components.color_text.color_text import color_text
+from components.pagination.pagination import Pagination
+from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
 class UserAppealPageUser:
+    @log_decorator
+    def switch_region(self) -> bool or list:
+        print(color_text("Switch region", color='blue'))
+        pagination = Pagination(table_name='regions', table_keys=['id', 'name'],
+                                display_keys=["ID", "Region Name"])
+        if not pagination.page_tab():
+            print("Region not found")
+            return False
+        region_id: int = int(input("Enter region ID: ").strip())
+        print(color_text("Checked...", color='cyan'))
+        query = '''
+            SELECT * FROM regions WHERE id=%s;
+            '''
+        result_get = execute_query(query, (region_id,), fetch='one')
+        if result_get is None:
+            print("Region not found")
+            return False
+        return result_get
+
     @log_decorator
     def send_request(self):
         pass

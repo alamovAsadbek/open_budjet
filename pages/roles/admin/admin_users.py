c840bb876072b6683ee0d6b1fb4f80b0ed5d36aa
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 23:43:53 +0500

    change

--- pages/roles/admin/admin_users.py
+++ pages/roles/admin/admin_users.py
@@ -1,4 +1,5 @@
 import hashlib
+import threading
 
 from components.color_text.color_text import color_text
 from components.pagination.pagination import Pagination
@@ -35,4 +36,10 @@ class AdminUsersPageAdmin:
             print(color_text('Passwords do not match', 'yellow'))
             password = hashlib.sha256(input("Enter password").strip().encode('utf-8')).hexdigest()
             confirm_password = hashlib.sha256(input("Confirm password: ").strip().encode('utf-8')).hexdigest()
+        query = '''
+        UPDATE users SET password=%s WHERE id=%s;
+        '''
+        params = (password, user_id)
+        threading.Thread(target=execute_query, args=(query, params)).start()
         print(color_text('User password update', 'green'))
+        return True

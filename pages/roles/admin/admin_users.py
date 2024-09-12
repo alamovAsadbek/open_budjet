728352cb00f634b1d06a22cee598594d6ec70760
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 23:50:45 +0500

    change show_all_users func

--- pages/roles/admin/admin_users.py
+++ pages/roles/admin/admin_users.py
@@ -10,6 +10,7 @@ from main_files.decorator.decorator_func import log_decorator
 class AdminUsersPageAdmin:
     @log_decorator
     def show_all_users(self):
+        print(color_text('Waiting...', 'cyan'))
         pagination = Pagination(table_name='users', table_keys=['id', 'first_name', 'last_name', 'email', 'created_at'],
                                 display_keys=['ID', 'First name', 'Last name', 'Email', 'Registered'])
         if not pagination.page_tab():

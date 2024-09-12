e8210832f06cbdd42373997c86393e485611931a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 16:32:49 +0500

    change pagination

--- components/pagination/pagination.py
+++ components/pagination/pagination.py
@@ -40,9 +40,9 @@ class Pagination:
                 for display_key, table_key in zip(self.display_keys, self.table_keys):
                     print(f"{display_key}: {data[f'{table_key}']}")
             print(
-                f"\n{color_text('1', color='blue', is_bold=True)}"
+                f"\n{color_text('1', color='blue', is_bold=True)} - Previous page\t"
                 f"<- {page_number}/{math.ceil(len(datas) / page_size)} -> "
-                f"{color_text('2', color='blue', is_bold=True)}\n")
+                f"\t{color_text('2', color='blue', is_bold=True)} - Next page\n")
             choice = input("Manage pagination, type exit to exit: ").strip()
             if choice == "exit":
                 return True

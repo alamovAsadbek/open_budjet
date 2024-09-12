8955c2c3fb6c0193378b1b3ed6c4684e5ade9884
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 10:38:21 +0500

    create connect_email context menager func

--- components/email_sender/email_sender.py
+++ components/email_sender/email_sender.py
@@ -1,7 +1,14 @@
+import contextlib
+
 from main_files.decorator.decorator_func import log_decorator
 
 
 class EmailSender:
+    @log_decorator
+    @contextlib.contextmanager
+    def connect_email(self):
+        pass
+
     @log_decorator
     def send_email(self):
         pass

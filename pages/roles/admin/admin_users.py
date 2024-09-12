218b41d1e112bed4d3d05e48b3e7b96030cf5e18
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 22:43:37 +0500

    change update_users func

--- pages/roles/admin/admin_users.py
+++ pages/roles/admin/admin_users.py
@@ -1,7 +1,13 @@
+from components.color_text.color_text import color_text
+from components.pagination.pagination import Pagination
 from main_files.decorator.decorator_func import log_decorator
 
 
 class AdminUsersPageAdmin:
     @log_decorator
-    def update_users(self):
-        pass
+    def update_users(self) -> bool:
+        print(color_text('Waiting...', 'cyan'))
+        pagination = Pagination(table_name='users', table_keys=['id', 'first_name', 'last_name', 'email', 'created_at'],
+                                display_keys=['ID', 'First name', 'Last name', 'Email', 'Registered'])
+        if not pagination.page_tab():
+            return False

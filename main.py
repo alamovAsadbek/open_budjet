9f524f568cccaaf5b248b670dd603cdb5a8951c0
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 15:42:11 +0500

    change admin_seasons_menu func

--- main.py
+++ main.py
@@ -132,7 +132,15 @@ def admin_seasons_menu():
     '''
     print(text)
     try:
-        pass
+        user_input: int = int(input("Choose menu: ").strip())
+        if user_input == 1:
+            pass
+        elif user_input == 2:
+            pass
+        elif user_input == 3:
+            pass
+        elif user_input == 4:
+            admin_menu()
     except Exception as e:
         print(f'Error: {e}')
         admin_seasons_menu()

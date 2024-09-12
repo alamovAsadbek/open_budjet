2c4168dc1380d476249173d3031b5383f16e360b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 15:58:34 +0500

    change code in admin.py

--- pages/roles/admin/admin.py
+++ pages/roles/admin/admin.py
@@ -1,3 +1,6 @@
+from pages.roles.admin.admin_seasons import AdminSeasonsPageAdmin
+
+
 class Admin:
     def __init__(self):
-        self.__season_menu = 0
+        self.__season_menu = AdminSeasonsPageAdmin()

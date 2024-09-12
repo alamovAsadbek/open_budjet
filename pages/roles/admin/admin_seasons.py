3f8255a8a607b1c8df14bba014fd378e1cfedc18
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 19:36:22 +0500

    create check_seasons func

--- pages/roles/admin/admin_seasons.py
+++ pages/roles/admin/admin_seasons.py
@@ -3,5 +3,9 @@ from main_files.decorator.decorator_func import log_decorator
 
 class AdminSeasonsPageAdmin:
     @log_decorator
+    def check_seasons(self):
+        pass
+
+    @log_decorator
     def create_seasons(self):
         pass

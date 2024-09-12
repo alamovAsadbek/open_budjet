973168e19be8123896dce39d9a5b21189e4094c7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 12:06:46 +0500

    change color_text func

--- components/color_text/color_text.py
+++ components/color_text/color_text.py
@@ -1,6 +1,28 @@
+from colorama import init, Fore, Style
+
 from main_files.decorator.decorator_func import log_decorator
+
+init(autoreset=True)
 
 
 @log_decorator
-def color_text():
-    pass
+def color_text(text, color, is_bold=False):
+    text = f"{Fore.RED}{text}{Style.RESET_ALL}"
+    if color == 'green':
+        text = f"{Fore.GREEN}{text}{Style.RESET_ALL}"
+    elif color == 'yellow':
+        text = f"{Fore.YELLOW}{text}{Style.RESET_ALL}"
+    elif color == 'blue':
+        text = f"{Fore.BLUE}{text}{Style.RESET_ALL}"
+    elif color == 'magenta':
+        text = f"{Fore.MAGENTA}{text}{Style.RESET_ALL}"
+    elif color == 'cyan':
+        text = f"{Fore.CYAN}{text}{Style.RESET_ALL}"
+    elif color == 'white':
+        text = f"{Fore.WHITE}{text}{Style.RESET_ALL}"
+    else:
+        text = f"{text}{Style.RESET_ALL}"
+    if is_bold:
+        text = f'{Style.BRIGHT}{text}{Style.RESET_ALL}'
+
+    return text

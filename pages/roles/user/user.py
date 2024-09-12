9f5f40c225a9f9bbf635829f5b740198086aae7f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-13 00:43:33 +0500

    change __init__ func

--- pages/roles/user/user.py
+++ pages/roles/user/user.py
@@ -1,9 +1,10 @@
 from main_files.decorator.decorator_func import log_decorator
+from pages.roles.user.user_appeal import UserAppealPageUser
 
 
 class User:
     def __init__(self):
-        pass
+        self.__appeals_menu = UserAppealPageUser()
 
     @log_decorator
     def send_request(self):

b4c807a00a56a1fdd24e74e858f4effc53c693d6
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 10:25:18 +0500

    change logout func

--- pages/auth/auth.py
+++ pages/auth/auth.py
@@ -1,6 +1,7 @@
 import threading
 
 from components.tables.tables import Tables
+from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
@@ -9,17 +10,22 @@ class Auth:
         self.__tables = Tables()
 
     @log_decorator
-    def create_tables(self):
+    def create_tables(self) -> bool:
         self.__tables.create_users_table()
         threading.Thread(target=self.__tables.create_categories_table).start()
+        return True
 
     @log_decorator
     def login(self):
         pass
 
     @log_decorator
-    def logout(self):
+    def logout(self) -> bool:
         self.create_tables()
+        query = '''
+               UPDATE users SET is_login=FALSE;
+        '''
+        execute_query(query)
         return True
 
     @log_decorator

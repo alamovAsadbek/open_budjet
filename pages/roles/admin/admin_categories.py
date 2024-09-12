4106a85e16e5d576bea096e5372a20ef555afdae
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 16:08:03 +0500

    change create_category func

--- pages/roles/admin/admin_categories.py
+++ pages/roles/admin/admin_categories.py
@@ -1,7 +1,16 @@
+import threading
+
+from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
 class AdminCategoryPageAdmin:
     @log_decorator
-    def create_category(self):
-        pass
+    def create_category(self) -> bool:
+        name: str = input("Enter category name: ").strip()
+        query = '''
+        INSERT INTO categories (name) VALUES (%s);
+        '''
+        threading.Thread(target=execute_query, args=(query, (name,))).start()
+        print("Category created successfully")
+        return True

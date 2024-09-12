3280b03abbe4eb51da6abad7b64806550d204c36
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 10:20:33 +0500

    change code in tables.py

--- components/tables/tables.py
+++ components/tables/tables.py
@@ -7,7 +7,7 @@ class Tables:
     def create_users_table(self):
         query = '''
         CREATE TABLE IF NOT EXISTS users (
-        ID INTEGER PRIMARY KEY,
+        ID BIGSERIAL PRIMARY KEY,
         FIRST_NAME VARCHAR(255) NOT NULL,
         LAST_NAME VARCHAR(255) NOT NULL,
         EMAIL VARCHAR(255) NOT NULL,

05e323185917337e5e56086d4a08d1b43692356a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 10:16:07 +0500

    change sql file

--- components/queries/queries.sql
+++ components/queries/queries.sql
@@ -1,4 +1,32 @@
 -- users tableni yaratish uchun query
+CREATE TABLE IF NOT EXISTS users
+(
+    ID
+    INTEGER
+    PRIMARY
+    KEY,
+    FIRST_NAME
+    VARCHAR
+(
+    255
+) NOT NULL,
+    LAST_NAME VARCHAR
+(
+    255
+) NOT NULL,
+    EMAIL VARCHAR
+(
+    255
+) NOT NULL,
+    PASSWORD VARCHAR
+(
+    255
+) NOT NULL,
+    IS_LOGIN BOOLEAN NOT NULL DEFAULT FALSE,
+    CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+
+-- categories tableni yaratish uchun query
 CREATE TABLE IF NOT EXISTS categories
 (
     ID
@@ -11,4 +39,4 @@ CREATE TABLE IF NOT EXISTS categories
     255
 ) NOT NULL,
     STATUS BOOLEAN NOT NULL DEFAULT TRUE
-    );
+    )

67eec47043fac895c98909ef2af46acf94908fa3
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 15:01:09 +0500

    create create_regions_table func

--- components/tables/tables.py
+++ components/tables/tables.py
@@ -32,5 +32,9 @@ class Tables:
         return True
 
     @log_decorator
+    def create_regions_table(self) -> bool:
+        pass
+
+    @log_decorator
     def create_appeals_table(self) -> bool:
         pass

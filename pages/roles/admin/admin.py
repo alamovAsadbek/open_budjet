c3bde04de3ed001786e790ee580b4e4338c9d308
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-12 16:01:45 +0500

    change __init__ func

--- pages/roles/admin/admin.py
+++ pages/roles/admin/admin.py
@@ -1,6 +1,12 @@
+from pages.roles.admin.admin_appeals import AdminAppealsPageAdmin
+from pages.roles.admin.admin_categories import AdminCategoryPageAdmin
 from pages.roles.admin.admin_seasons import AdminSeasonsPageAdmin
+from pages.roles.admin.admin_users import AdminUsersPageAdmin
 
 
 class Admin:
     def __init__(self):
         self.__season_menu = AdminSeasonsPageAdmin()
+        self.__appeals_menu = AdminAppealsPageAdmin()
+        self.__categories_menu = AdminCategoryPageAdmin()
+        self.__users_menu = AdminUsersPageAdmin()

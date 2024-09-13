3e5f203f052d9d98d5a92c5ac79f853483ea4f80
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-13 08:22:04 +0500

    change get_active_appeal func

--- pages/roles/user/user_season.py
+++ pages/roles/user/user_season.py
@@ -1,10 +1,31 @@
+from main_files.database.db_setting import execute_query, get_active_user
 from main_files.decorator.decorator_func import log_decorator
 
 
 class UserSeason:
     @log_decorator
     def get_active_appeal(self):
-        pass
+        query = '''
+        SELECT a.id          as a_id,
+           a.name        as a_name,
+           a.description as a_description,
+           a.price       as a_price,
+           c.name        as category_name,
+           r.name        as region_name,
+           d.name        as districts_name,
+           s.name        as season_name,
+           s.status      as season_status,
+           s.created_at  as season_created
+        FROM appeals a
+                 inner join categories c on c.ID = a.CATEGORY_ID
+                 inner join districts d on a.DISTRICTS_ID = d.ID
+                 inner join regions r on d.REGION_ID = r.ID
+                 inner join SEASONS S on S.ID = a.SEASONS_ID
+        WHERE a.user_id = '%s' and s.status = 'vote';
+        '''
+        params = (get_active_user()['id'],)
+        result = execute_query(query, params, fetch='one')
+        return result
 
     @log_decorator
     def voting_user(self):

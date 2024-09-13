cc1abbe3d246d25c6ba1bad9721c7ed64ad02b71
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-13 11:07:44 +0500

    change

--- pages/roles/admin/admin_appeals.py
+++ pages/roles/admin/admin_appeals.py
@@ -15,39 +15,40 @@ class AdminAppealsPageAdmin:
         return execute_query(query, fetch='one')
 
     @log_decorator
-    def get_appeals(self, status: str):
+    def get_appeals(self, status: str, data_id: int = None):
         active_season = self.get_active_season()
         if active_season is None or active_season is False:
             print(color_text('Active season not found!', 'yellow'))
             return False
-        query = '''
-        select a.id          as a_id,
-           a.name        as a_name,
-           a.status      as a_status,
-           a.description as a_description,
-           a.price       as a_price,
-           c.name        as category_name,
-           r.name        as region_name,
-           d.name        as districts_name,
-           s.name        as season_name,
-           s.status      as season_status,
-           s.created_at  as season_created,
-           u.first_name  as u_first_name,
-           u.last_name   as u_last_name,
-           u.email       as u_email,
-           a.seasons_id  as a_seasons_id
-        FROM appeals a
-                 inner join categories c on c.ID = a.CATEGORY_ID
-                 inner join districts d on a.DISTRICTS_ID = d.ID
-                 inner join regions r on d.REGION_ID = r.ID
-                 inner join SEASONS S on S.ID = a.SEASONS_ID
-                 inner join users u on u.ID = a.USER_ID
-        where s.status = 'appeal'
-          and a.seasons_id = %s
-          and a.status = %s;
-        '''
-        param = (active_season['id'], status)
-        return execute_query(query, param, fetch='all')
+        if data_id is None:
+            query = '''
+            select a.id          as a_id,
+               a.name        as a_name,
+               a.status      as a_status,
+               a.description as a_description,
+               a.price       as a_price,
+               c.name        as category_name,
+               r.name        as region_name,
+               d.name        as districts_name,
+               s.name        as season_name,
+               s.status      as season_status,
+               s.created_at  as season_created,
+               u.first_name  as u_first_name,
+               u.last_name   as u_last_name,
+               u.email       as u_email,
+               a.seasons_id  as a_seasons_id
+            FROM appeals a
+                     inner join categories c on c.ID = a.CATEGORY_ID
+                     inner join districts d on a.DISTRICTS_ID = d.ID
+                     inner join regions r on d.REGION_ID = r.ID
+                     inner join SEASONS S on S.ID = a.SEASONS_ID
+                     inner join users u on u.ID = a.USER_ID
+            where s.status = 'appeal'
+              and a.seasons_id = %s
+              and a.status = %s;
+            '''
+            param = (active_season['id'], status)
+            return execute_query(query, param, fetch='all')
 
     @log_decorator
     def show_appeals(self, status):

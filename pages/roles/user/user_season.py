4100d5935061142893425de9f013f6543080b09b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-13 08:19:10 +0500

    create get_active_appeal func

--- pages/roles/user/user_season.py
+++ pages/roles/user/user_season.py
@@ -3,5 +3,9 @@ from main_files.decorator.decorator_func import log_decorator
 
 class UserSeason:
     @log_decorator
+    def get_active_appeal(self):
+        pass
+
+    @log_decorator
     def voting_user(self):
         pass

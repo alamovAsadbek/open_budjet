6e0b164beda483006937ef3f95afc20e3c2aee0a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-13 09:52:32 +0500

    change sql file

--- components/queries/queries.sql
+++ components/queries/queries.sql
@@ -396,3 +396,24 @@ FROM appeals a
 WHERE a.status = 'accepted'
   and s.id = '% s'
   and c.id = '% s';
+
+-- Admin uchun barcha murojaatlarni ko'rish uchun query
+select a.id          as a_id,
+       a.name        as a_name,
+       a.description as a_description,
+       a.price       as a_price,
+       c.name        as category_name,
+       r.name        as region_name,
+       d.name        as destricts_name,
+       s.name        as season_name,
+       s.status      as season_status,
+       s.created_at  as season_created,
+       u.first_name  as u_first_name,
+       u.last_name   as u_last_name,
+       u.email       as u_email
+FROM appeals a
+         inner join categories c on c.ID = a.CATEGORY_ID
+         inner join districts d on a.DISTRICTS_ID = d.ID
+         inner join regions r on d.REGION_ID = r.ID
+         inner join SEASONS S on S.ID = a.SEASONS_ID
+         inner join users u on u.ID = a.USER_ID;

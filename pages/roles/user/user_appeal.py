c59c6310581baab1aacb36a5ddc729d0b74c9da8
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-13 07:45:41 +0500

    change get_my_request func

--- pages/roles/user/user_appeal.py
+++ pages/roles/user/user_appeal.py
@@ -79,7 +79,26 @@ class UserAppealPageUser:
 
     @log_decorator
     def get_my_request(self):
-        pass
+        query = '''
+        SELECT a.id          as a_id,
+           a.name        as a_name,
+           a.description as a_description,
+           a.price       as a_price,
+           c.name        as category_name,
+           r.name        as region_name,
+           d.name        as destricts_name,
+           s.name        as season_name,
+           s.status      as season_status,
+           s.created_at  as season_created
+        FROM appeals a
+                 inner join categories c on c.ID = a.CATEGORY_ID
+                 inner join districts d on a.DISTRICTS_ID = d.ID
+                 inner join regions r on d.REGION_ID = r.ID
+                 inner join SEASONS S on S.ID = a.SEASONS_ID
+        WHERE a.user_id = '%s';
+        '''
+        params = (get_active_user(['id']),)
+        return execute_query(query, params, fetch='all')
 
     @log_decorator
     def send_request(self):

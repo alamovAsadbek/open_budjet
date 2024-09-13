795de81d7793026dcb15abcb7fdcd66c95805999
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-13 09:46:04 +0500

    change sql file

--- components/queries/queries.sql
+++ components/queries/queries.sql
@@ -373,3 +373,26 @@ WHERE a.user_id = '%s';
 SELECT *
 FROM seasons
 WHERE status = 'vote';
+
+-- Tasdiqlangan murojaatlarni ko'rish uchun query
+SELECT a.id          as a_id,
+       a.name        as a_name,
+       a.description as a_description,
+       a.price       as a_price,
+       a.status      as a_status,
+       c.id          as category_id,
+       c.name        as category_name,
+       r.name        as region_name,
+       d.name        as districts_name,
+       s.id          as season_id,
+       s.name        as season_name,
+       s.status      as season_status,
+       s.created_at  as season_created
+FROM appeals a
+         inner join categories c on c.ID = a.CATEGORY_ID
+         inner join districts d on a.DISTRICTS_ID = d.ID
+         inner join regions r on d.REGION_ID = r.ID
+         inner join SEASONS S on S.ID = a.SEASONS_ID
+WHERE a.status = 'accepted'
+  and s.id = '% s'
+  and c.id = '% s';

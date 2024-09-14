b750f8f47b4cccf9a918376ab582b12c033c082b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-14 11:00:51 +0500

    change code

--- pages/roles/user/user_profile.py
+++ pages/roles/user/user_profile.py
@@ -1,3 +1,6 @@
+from main_files.database.db_setting import get_active_user
+
+
 class UserProfile:
     def __init__(self):
-        self.__active_user = 0
+        self.__active_user = get_active_user()

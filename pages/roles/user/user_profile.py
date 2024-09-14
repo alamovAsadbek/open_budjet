324606e7af5e8735bf2c46aa30f2b0e14cdae96d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-14 11:46:30 +0500

    change

--- pages/roles/user/user_profile.py
+++ pages/roles/user/user_profile.py
@@ -6,9 +6,12 @@ from main_files.decorator.decorator_func import log_decorator
 
 
 class UserProfile:
+    def __init__(self):
+        self.__active_user = get_active_user()
+
     @log_decorator
     def show_profile(self) -> bool:
-        print(get_active_user())
+        print(self.__active_user)
         while True:
             update_check = input('Do you want to update your profile (y/n): ').strip().lower()
             if update_check == 'y':

e981b53f6262beb288a8a1e6791138e72708af03
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-14 11:02:19 +0500

    create show_profile func

--- pages/roles/user/user_profile.py
+++ pages/roles/user/user_profile.py
@@ -1,6 +1,11 @@
 from main_files.database.db_setting import get_active_user
+from main_files.decorator.decorator_func import log_decorator
 
 
 class UserProfile:
     def __init__(self):
         self.__active_user = get_active_user()
+
+    @log_decorator
+    def show_profile(self):
+        pass

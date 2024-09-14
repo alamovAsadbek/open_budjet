33f984dff89a3215dfad0389608351a6797fcc44
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-14 11:31:56 +0500

    change show_profile func

--- pages/roles/user/user_profile.py
+++ pages/roles/user/user_profile.py
@@ -12,11 +12,18 @@ class UserProfile:
     @log_decorator
     def show_profile(self) -> bool:
         print(self.__active_user)
+        while True:
+            update_check = input('Do you want to update your profile (y/n): ').strip().lower()
+            if update_check == 'y':
+                self.update_profile()
+            elif update_check == 'n':
+                break
+            else:
+                print(color_text('Wrong input', 'yellow'))
         return True
 
     @log_decorator
     def update_profile(self) -> bool:
-        self.show_profile()
         password: str = hashlib.sha256(input("Enter new password: ").strip().encode('utf-8')).hexdigest()
         confirm_password: str = hashlib.sha256(input("Confirm password: ").strip().encode('utf-8')).hexdigest()
         while password != confirm_password:
